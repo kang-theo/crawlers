13decff245c58f4b963bc6212e005d7bced53b48
kang-theo <theo.cy.kang@gmail.com>
2024-04-08 10:36:28 +0800

    Update selenium test

--- misc/selenium_test.py
+++ misc/selenium_test.py
@@ -1,14 +1,20 @@
 """
 selenium will use chromedriver to open a chrome instance and execute the request defined
+chromedriver can be put in python bin directory
 if a chrome browser is opened, the basic functionality of selenium and chromedriver is fine.
 """
+
+# minimal example, open a chrome browser
+# from selenium import webdriver
+# driver = webdriver.Chrome('chromedriver')
+# input()
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('chromedriver')
 driver.get("https://www.python.org")
 print(driver.title)
-# input()
 search_bar = driver.find_element_by_name("q")
 search_bar.clear()
 search_bar.send_keys("getting started with python")
